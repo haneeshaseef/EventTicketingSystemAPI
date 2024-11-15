682edfdceb41d296633eddb287dbaf4ceb51bca7
haneeshaseef <haneeshaseef521@gmail.com>
2024-11-15 20:40:29 +0530

    first commit

---
+++ src/main/java/org/coursework/eventticketingsystemapi/EventTicketingSystemApiApplication.java
@@ -0,0 +1,13 @@
+package org.coursework.eventticketingsystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicketingSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventTicketingSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/coursework/eventticketingsystemapi/EventTicketingSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coursework.eventticketingsystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketingSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
